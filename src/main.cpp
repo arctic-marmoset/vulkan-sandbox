0fad381ffd559b9ea226ea94db4a6c76cd9214c5
arctic-marmoset <45583929+arctic-marmoset@users.noreply.github.com>
2021-03-10 23:29:02 -0500

    Add window initialisation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,62 @@
+#include <GLFW/glfw3.h>
+
 #include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+
+constexpr std::uint32_t window_width = 1280;
+constexpr std::uint32_t window_height = 720;
+
+class application
+{
+public:
+    void run()
+    {
+        init_window();
+        main_loop();
+        cleanup();
+    }
+
+private:
+    void init_window()
+    {
+        if (!glfwInit())
+            throw std::runtime_error("Failed to initialize GLFW!");
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window_ = glfwCreateWindow(window_width, window_height, "Vulkan Renderer", nullptr, nullptr);
+    }
+
+    void main_loop()
+    {
+        while (!glfwWindowShouldClose(window_)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup()
+    {
+        glfwDestroyWindow(window_);
+        glfwTerminate();
+    }
+
+private:
+    GLFWwindow *window_ = nullptr;
+};
 
 int main()
 {
+    ::application app;
+
+    try {
+        app.run();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << '\n';
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
