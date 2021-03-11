0e94f48e9f17aa51eaeb2ef9fb4929053398537e
arctic-marmoset <45583929+arctic-marmoset@users.noreply.github.com>
2021-03-11 00:00:58 -0500

    Add Vulkan instance initialisation

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan.hpp>
 
 #include <cstdlib>
 #include <exception>
 #include <iostream>
 #include <stdexcept>
 
-constexpr std::uint32_t window_width = 1280;
+constexpr std::uint32_t window_width  = 1280;
 constexpr std::uint32_t window_height = 720;
 
 class application
@@ -14,6 +15,7 @@ public:
     void run()
     {
         init_window();
+        init_vulkan();
         main_loop();
         cleanup();
     }
@@ -30,6 +32,33 @@ private:
         window_ = glfwCreateWindow(window_width, window_height, "Vulkan Renderer", nullptr, nullptr);
     }
 
+    void init_vulkan()
+    {
+        create_instance();
+    }
+
+    void create_instance()
+    {
+        const vk::ApplicationInfo appInfo = {
+            .pApplicationName   = "Hello Triangle",
+            .applicationVersion = VK_MAKE_VERSION(0, 1, 0),
+            .pEngineName        = "Sandbox",
+            .engineVersion      = VK_MAKE_VERSION(0, 1, 0),
+            .apiVersion         = VK_API_VERSION_1_0,
+        };
+
+        std::uint32_t glfwExtensionCount = 0;
+        const char **glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        const vk::InstanceCreateInfo createInfo = {
+            .pApplicationInfo        = &appInfo,
+            .enabledExtensionCount   = glfwExtensionCount,
+            .ppEnabledExtensionNames = glfwExtensions,
+        };
+
+        instance_ = vk::createInstance(createInfo);
+    }
+
     void main_loop()
     {
         while (!glfwWindowShouldClose(window_)) {
@@ -39,12 +68,14 @@ private:
 
     void cleanup()
     {
+        instance_.destroy();
         glfwDestroyWindow(window_);
         glfwTerminate();
     }
 
 private:
     GLFWwindow *window_ = nullptr;
+    vk::Instance instance_;
 };
 
 int main()
