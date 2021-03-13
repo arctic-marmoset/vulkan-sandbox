075c06111021211ba497747ce223161da4acef0d
arctic-marmoset <45583929+arctic-marmoset@users.noreply.github.com>
2021-03-13 14:55:49 -0500

    Add validation layers

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,90 @@
 #include <GLFW/glfw3.h>
+#include <vulkan/vk_platform.h>
 #include <vulkan/vulkan.hpp>
 
+#include <algorithm>
+#include <cstdio>
 #include <cstdlib>
 #include <exception>
 #include <iostream>
 #include <stdexcept>
+#include <string_view>
 
 constexpr std::uint32_t window_width  = 1280;
 constexpr std::uint32_t window_height = 720;
+
+constexpr std::array validation_layers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifndef NDEBUG
+constexpr bool debug_mode = true;
+#else
+constexpr bool debug_mode = false;
+#endif
+
+VKAPI_ATTR vk::Bool32 VKAPI_CALL debug_callback(VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+                                                [[maybe_unused]] VkDebugUtilsMessageTypeFlagsEXT message_type,
+                                                const VkDebugUtilsMessengerCallbackDataEXT *data,
+                                                [[maybe_unused]] void *user_data)
+{
+    switch (message_severity) {
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+        std::printf("[DEBUG_CALLBACK] %s\n", data->pMessage);
+        break;
+
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_FLAG_BITS_MAX_ENUM_EXT:
+        std::fprintf(stderr, "[DEBUG_CALLBACK] %s\n", data->pMessage);
+        break;
+    }
+
+    return VK_FALSE;
+}
+
+bool validation_layers_supported()
+{
+    auto layers = vk::enumerateInstanceLayerProperties();
+    std::ranges::sort(layers, { }, &vk::LayerProperties::layerName);
+
+    const auto name_projection = [](const vk::LayerProperties &properties)
+    {
+        auto name = static_cast<std::string_view>(properties.layerName);
+        return name;
+    };
+
+    return std::ranges::includes(layers, validation_layers, { }, name_projection);
+}
+
+std::vector<const char *> required_extensions()
+{
+    std::uint32_t glfw_extension_count = 0;
+    const char **glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+
+    std::vector extensions(glfw_extensions, glfw_extensions + glfw_extension_count);
+
+    if constexpr (debug_mode)
+        extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+
+    return extensions;
+}
+
+void init_debug_messenger_create_info(vk::DebugUtilsMessengerCreateInfoEXT &createInfo)
+{
+    createInfo = {
+        .messageSeverity = vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose
+                         | vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning
+                         | vk::DebugUtilsMessageSeverityFlagBitsEXT::eError,
+
+        .messageType     = vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral
+                         | vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation
+                         | vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance,
+
+        .pfnUserCallback = debug_callback,
+    };
+}
 
 class application
 {
@@ -35,10 +112,20 @@ private:
     void init_vulkan()
     {
         create_instance();
+        attach_debug_messenger();
     }
 
     void create_instance()
     {
+        const vk::DynamicLoader loader;
+        auto *vkGetInstanceProcAddr = loader.getProcAddress<PFN_vkGetInstanceProcAddr>("vkGetInstanceProcAddr");
+        VULKAN_HPP_DEFAULT_DISPATCHER.init(vkGetInstanceProcAddr);
+
+        if constexpr (debug_mode) {
+            if (!validation_layers_supported())
+                throw std::runtime_error("Validation layers requested but not supported!");
+        }
+
         const vk::ApplicationInfo appInfo = {
             .pApplicationName   = "Hello Triangle",
             .applicationVersion = VK_MAKE_VERSION(0, 1, 0),
@@ -47,16 +134,39 @@ private:
             .apiVersion         = VK_API_VERSION_1_0,
         };
 
-        std::uint32_t glfwExtensionCount = 0;
-        const char **glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        vk::StructureChain<vk::InstanceCreateInfo, vk::DebugUtilsMessengerCreateInfoEXT> chain;
+        auto &createInfo      = chain.get<vk::InstanceCreateInfo>();
+        auto &debugCreateInfo = chain.get<vk::DebugUtilsMessengerCreateInfoEXT>();
 
-        const vk::InstanceCreateInfo createInfo = {
+        const auto extensions = required_extensions();
+
+        createInfo = {
             .pApplicationInfo        = &appInfo,
-            .enabledExtensionCount   = glfwExtensionCount,
-            .ppEnabledExtensionNames = glfwExtensions,
+            .enabledExtensionCount   = static_cast<std::uint32_t>(extensions.size()),
+            .ppEnabledExtensionNames = extensions.data(),
         };
 
+        if constexpr (debug_mode) {
+            init_debug_messenger_create_info(debugCreateInfo);
+            createInfo.enabledLayerCount   = static_cast<std::uint32_t>(validation_layers.size());
+            createInfo.ppEnabledLayerNames = validation_layers.data();
+        } else {
+            chain.unlink<vk::DebugUtilsMessengerCreateInfoEXT>();
+        }
+
         instance_ = vk::createInstance(createInfo);
+        VULKAN_HPP_DEFAULT_DISPATCHER.init(instance_);
+    }
+
+    void attach_debug_messenger()
+    {
+        if constexpr (!debug_mode)
+            return;
+
+        vk::DebugUtilsMessengerCreateInfoEXT createInfo;
+        init_debug_messenger_create_info(createInfo);
+
+        debug_messenger_ = instance_.createDebugUtilsMessengerEXT(createInfo);
     }
 
     void main_loop()
@@ -68,6 +178,9 @@ private:
 
     void cleanup()
     {
+        if constexpr (debug_mode)
+            instance_.destroy(debug_messenger_);
+
         instance_.destroy();
         glfwDestroyWindow(window_);
         glfwTerminate();
@@ -76,6 +189,9 @@ private:
 private:
     GLFWwindow *window_ = nullptr;
     vk::Instance instance_;
+#ifndef NDEBUG
+    vk::DebugUtilsMessengerEXT debug_messenger_;
+#endif
 };
 
 int main()
